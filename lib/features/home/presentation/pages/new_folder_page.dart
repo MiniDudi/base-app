81728687c28c2938e33872805857bc026da9f398
MiniDudi <duduhnpinese@gmail.com>
2026-05-26 10:08:16 -0300

    chore: new folder page

--- lib/features/home/presentation/pages/new_folder_page.dart
+++ lib/features/home/presentation/pages/new_folder_page.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../shared/constants/app_images.dart';
 import '../../../../shared/constants/app_text_styles.dart';
 import '../../../../shared/widgets/buttons/main_button.dart';
+import '../../../../shared/widgets/inputs/main_text_input.dart';
 
 class NewFolderPage extends StatefulWidget {
   const NewFolderPage({super.key});
@@ -13,13 +14,39 @@ class NewFolderPage extends StatefulWidget {
 }
 
 class _NewFolderPageState extends State<NewFolderPage> {
+  final TextEditingController _folderName = TextEditingController();
+  final TextEditingController _folderCategory = TextEditingController();
+  final TextEditingController _folderDescription = TextEditingController();
+
+  //  FOLDER USERS MUST BE A LIST OF USER IDs OR USER MODELS
+  //  final TextEditingController _folderUsers = TextEditingController();
+
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  void initState() {
+    // TODO: implement initState
+    super.initState();
   }
 
-  Widget _buildPage() {
-    return Padding(padding: const EdgeInsets.all(8.0), child: Column());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: _buildPage(context));
+  }
+
+  Widget _buildPage(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _buildReturnButton(context),
+          _buildMainText(context),
+          _buildFirstInputs(context),
+          _buildVisibilityOptions(context),
+          _buildLastInputs(context),
+        ],
+      ),
+    );
   }
 
   Widget _buildReturnButton(BuildContext context) {
@@ -44,6 +71,52 @@ class _NewFolderPageState extends State<NewFolderPage> {
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [Text('Dados do Folder', style: AppTextStyles.h1)],
+    );
+  }
+
+  Widget _buildFirstInputs(BuildContext context) {
+    return Container();
+  }
+
+  Widget _buildVisibilityOptions(BuildContext context) {
+    return Container();
+  }
+
+  Widget _buildLastInputs(BuildContext context) {
+    return Column(
+      children: [
+        MainTextInput(
+          title: 'Permissão para usuários (Opcional)',
+          hint: 'Selecione multiplos usuários',
+          enabled: true,
+          readOnly: false,
+          isRequired: true,
+          isPassword: false,
+        ),
+        MainTextInput(
+          title: 'Descrição',
+          hint: 'Descrição do Folder',
+          enabled: true,
+          readOnly: false,
+          isRequired: true,
+          isPassword: false,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMainButton(BuildContext context) {
+    return Row(
+      children: [
+        MainButton(
+          text: 'Configurações',
+          prefixIcon: AppImages.settingsIconSVG,
+          variant: ButtonVariant.white,
+          isLoading: false,
+          expand: false,
+          enable: true,
+        ),
+      ],
     );
   }
 }
