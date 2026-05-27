54d4b5531ce8466e3ae5e5d6b33ea6b7f3079de4
MiniDudi <duduhnpinese@gmail.com>
2026-05-27 10:37:08 -0300

    fix: folder visibility radius

--- lib/features/home/presentation/widgets/folder_visibility_check.dart
+++ lib/features/home/presentation/widgets/folder_visibility_check.dart
@@ -1,11 +1,16 @@
 import 'package:base_app/shared/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
-class FolderVisibilityCheck extends StatelessWidget {
+class FolderVisibilityCheck extends StatefulWidget {
   const FolderVisibilityCheck({super.key});
 
-  final bool isPrivateChecked = false;
-  final bool isPublicChecked = false;
+  @override
+  State<StatefulWidget> createState() => _StateFolderVisibilityCheck();
+}
+
+class _StateFolderVisibilityCheck extends State<FolderVisibilityCheck> {
+  late bool isPrivateChecked = false;
+  late bool isPublicChecked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -19,17 +24,32 @@ class FolderVisibilityCheck extends StatelessWidget {
   }
 
   Widget _buildCheckCard(BuildContext context, bool isPrivate, bool isPublic) {
-    return Container(
-      color: !isPrivateChecked && !isPublicChecked
-          ? AppColors.white
-          : AppColors.primary,
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Icon(isPrivate ? Icons.lock : Icons.person),
-          Text(isPrivate ? 'Privado' : 'Público'),
-        ],
+    return GestureDetector(
+      onTap: () {
+        if (isPrivate) {
+          setState(() {
+            isPrivateChecked = true;
+            isPublicChecked = false;
+          });
+        } else {
+          setState(() {
+            isPrivateChecked = false;
+            isPublicChecked = true;
+          });
+        }
+      },
+      child: Container(
+        color: !isPrivateChecked && !isPublicChecked
+            ? AppColors.white
+            : AppColors.primary,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Icon(isPrivate ? Icons.lock : Icons.person),
+            Text(isPrivate ? 'Privado' : 'Público'),
+          ],
+        ),
       ),
     );
   }
